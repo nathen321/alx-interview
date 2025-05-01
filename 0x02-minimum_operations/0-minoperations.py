4e5c0ccd4df762f99c3b0ea6e03a8f2c9a23af09
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-01 21:12:45 +0000

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""method that calculates the fewest number 
+of operations needed to result in exactly n H
+"""
+
+def minOperations(n: int) -> int:
+    """The minimum number of operations needed to achieve exactly 
+    n 'H's is the sum of the prime factors of n.
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        divisor += 1
+    return operations
